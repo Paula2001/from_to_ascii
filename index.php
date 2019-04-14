1093b8e2817c8829c32854943030834af3b6b488
polagorge <polagorge@gmail.com>
2019-04-14 14:11:52 -0700

    numaric is added

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ function convertToString( $number){
     for($i = 0; $i < strlen($number_string);$i++){
             $number .= $number_string[$i];
 
-        if(ctype_alpha(chr($number)) || $number == 32) {
+        if(ctype_alpha(chr($number)) || is_numeric(chr($number)) || $number == 32) {
             echo $number ;
             echo "<br>";
             $string .= chr($number);
@@ -27,10 +27,10 @@ function convertToASCII($string){
     return $number ;
 }
 
-echo convertToString("1161011151161051101033211610410511532116101120116");
+echo convertToString("841041051153211910198115105116101324950513210511532100111119110321171101161051083212111111732999710810832109117989711510410111432105110102111114109971161051111103211610199104110111108111103121321161113210711011111932109111114101324950515151");
 echo "<br>";
 
-echo convertToASCII('testing this text');
+echo convertToASCII('This website 123 is down until you call mubasher information technology to know more 12333');
 echo "<br>";
 
 
